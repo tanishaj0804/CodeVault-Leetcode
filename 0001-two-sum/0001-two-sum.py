9cb1f98f27d8ac75c60677d0d75405dfd430685f
tanishaj0804 <atanishajain08@gmail.com>
2026-07-26 19:16:56 +0530

    Time: 0 ms (100%), Space: 13.2 MB (52.7%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [seen[diff],i] 
+            seen[nums[i]] = i
