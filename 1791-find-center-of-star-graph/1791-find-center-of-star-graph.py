796ae27ea08eeb112854de9652422519e49ac3b1
tanishaj0804 <atanishajain08@gmail.com>
2026-07-30 19:23:37 +0530

    Time: 0 ms (100%), Space: 33 MB (80.4%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-        common = set(edges[0])  # the first row
-        for row in edges:
-            common &= set(row)   # {1,2} & {2,3} = 2
-        ans = common.pop()
-        return ans
+        if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
+            return edges[0][0]
+        else:
+            return edges[0][1]
+        
