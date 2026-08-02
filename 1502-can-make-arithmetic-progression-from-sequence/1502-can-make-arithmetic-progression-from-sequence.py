dd81c4877cdd987b2d2b9e67e9129f77f791a3a5
tanishaj0804 <atanishajain08@gmail.com>
2026-08-02 17:17:31 +0530

    Time: 4 ms (15.44%), Space: 12.3 MB (99.11%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -4,13 +4,8 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        n = len(arr)
-        for i in range(n-1):
-            minv = i
-            for j in range(i+1,n):
-                if arr[j] < arr[minv]:
-                    minv = j
-            arr[i],arr[minv] = arr[minv],arr[i]
+        n=len(arr)
+        arr.sort()
         diff = arr[1] - arr[0]
         for i in range(1,n-1):
             if arr[i+1] - arr[i] != diff:
