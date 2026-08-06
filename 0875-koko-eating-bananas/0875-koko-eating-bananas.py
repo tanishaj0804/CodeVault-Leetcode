dbb021309784021cee7373e066b918fe69417adb
tanishaj0804 <atanishajain08@gmail.com>
2026-08-06 17:43:30 +0530

    Time: 167 ms (51.22%), Space: 13.4 MB (48.8%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -7,16 +7,18 @@ class Solution(object):
         """
         if len(piles) == h:
             return max(piles)
-        l,r = 1,max(piles)
-        ans = r
-        while l<=r:
-            mid = (l+r)//2
-            hours = sum((p+mid-1)//mid for p in piles)
-            if hours <= h:
-                ans = mid
-                r = mid-1
+        minv = 1
+        maxv = ans = max(piles)
+        while minv <= maxv:
+            mid = (minv+maxv) // 2
+            hours = 0
+            for pile in piles:
+                hours += (pile+mid-1)//mid
+            if hours > h:
+                minv = mid+1
             else:
-                l = mid+1
+                ans = mid
+                maxv = mid-1
         return ans
         
         
