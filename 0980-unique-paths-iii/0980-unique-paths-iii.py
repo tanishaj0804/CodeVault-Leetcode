f0ff1970cce6e26c541191d6554e541589f80f1b
tanishaj0804 <atanishajain08@gmail.com>
2026-08-07 15:54:41 +0530

    Time: 21 ms (26.14%), Space: 12.4 MB (56.21%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def uniquePathsIII(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m = len(grid)
+        n = len(grid[0])
+        sr = sc = er = ec = 0 
+        visited = set()
+        self.output = 0
+        empty = 0
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        def dfs(x,y,visited,e):
+            if x == er and y == ec:
+                if e == empty+1:
+                    self.output += 1
+                return
+            if 0<=x<m and 0<=y<n and grid[x][y] != -1 and (x,y) not in visited:
+                visited.add((x,y))
+                for dr,dc in directions:
+                    dfs(dr+x,dc+y,visited,e+1)
+                visited.remove((x,y))
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    er,ec = i,j
+                elif grid[i][j] == 1:
+                    sr,sc = i,j
+                elif grid[i][j] == 0:
+                    empty += 1
+        dfs(sr,sc,visited,0)
+        return self.output
+        
+            
+
+        
