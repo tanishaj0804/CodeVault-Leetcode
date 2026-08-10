b038352be2b28eae8e50a880d1b1657d384edb77
tanishaj0804 <atanishajain08@gmail.com>
2026-08-10 13:53:06 +0530

    Time: 1202 ms (32.73%), Space: 161.5 MB (10.61%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,11 +1,11 @@
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
         @cache
-        def dfs(i):
+        def dfs(i):   # if i have i stones, can i win
             if i == 0:
                 return False
             for j in range(1,isqrt(i)+1):
-                if not dfs(i-j**2):
+                if not dfs(i-j**2):  #currently at ith stone and u decide to remove j**2 stones
                     return True
             return False
         return dfs(n)
