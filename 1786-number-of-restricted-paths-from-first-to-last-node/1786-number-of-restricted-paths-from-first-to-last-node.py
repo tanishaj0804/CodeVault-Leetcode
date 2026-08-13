ab159c02b1552935fa3ee00d5a4a5537b8711e94
tanishaj0804 <atanishajain08@gmail.com>
2026-08-13 15:20:25 +0530

    Time: 474 ms (27.66%), Space: 52.9 MB (68.08%) - LeetHub

--- 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
@@ -1,60 +1,31 @@
 class Solution:
     def countRestrictedPaths(self, n, edges):
-
-        MOD = 10**9 + 7
-
-        # Build graph
-        adj = [[] for _ in range(n + 1)]
-
-        for u, v, w in edges:
-            adj[u].append((v, w))
-            adj[v].append((u, w))
-
-        # -------------------------
-        # Dijkstra from node n
-        # -------------------------
-
-        dist = [float('inf')] * (n + 1)
+        MOD = 10**9+7
+        adj =[[] for _ in range(n+1)]
+        for u,v,w in edges:
+            adj[u].append((v,w))
+            adj[v].append((u,w))
+        dist = [float('inf') for _ in range(n+1)]
         dist[n] = 0
-
-        heap = [(0, n)]
-
+        heap = [(0,n)]
         while heap:
-
-            d, node = heapq.heappop(heap)
-
+            d,node = heapq.heappop(heap)
             if d > dist[node]:
                 continue
-
-            for nei, w in adj[node]:
-
-                new_dist = d + w
-
-                if new_dist < dist[nei]:
-                    dist[nei] = new_dist
-                    heapq.heappush(heap, (new_dist, nei))
-
-        # -------------------------
-        # DP / DFS
-        # -------------------------
-
-        dp = [-1] * (n + 1)
+            for nei,w in adj[node]:
+                neww = w+d
+                if neww < dist[nei]:
+                    dist[nei] = neww
+                    heapq.heappush(heap,(neww,nei))
+        dp = [-1]*(n+1)
         dp[n] = 1
-
         def dfs(node):
-
             if dp[node] != -1:
                 return dp[node]
-
             ways = 0
-
-            for nei, w in adj[node]:
-
+            for nei,w in adj[node]:
                 if dist[node] > dist[nei]:
                     ways += dfs(nei)
-
-            dp[node] = ways % MOD
-
+            dp[node] = ways%MOD
             return dp[node]
-
         return dfs(1)
