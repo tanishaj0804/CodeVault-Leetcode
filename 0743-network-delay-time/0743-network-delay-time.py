dcef33c2f0362b9c22b5e7056b85438e6dc98a14
tanishaj0804 <atanishajain08@gmail.com>
2026-08-13 14:01:19 +0530

    Time: 332 ms (77.72%), Space: 21.1 MB (77.95%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,26 +1,20 @@
-class Solution(object):
-    def networkDelayTime(self, times, n, k):
-        """
-        :type times: List[List[int]]
-        :type n: int
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adj = [[] for _ in range(n+1)]
         for u,v,w in times:
             adj[u].append((v,w))
-        dist = [float('inf')]*(n+1)
-        heap = [(0,k)]
+        dist =[float('inf')]*(n+1)
         dist[k] = 0
+        heap = [(0,k)]
         while heap:
-            time,node = heapq.heappop(heap)
-            if time > dist[node]:
-                continue
-            for nei,wt in adj[node]:
-                newt = time+wt
-                if newt < dist[nei]:
-                    dist[nei] = newt
-                    heapq.heappush(heap,(newt,nei))
+            cost,node = heapq.heappop(heap)
+            if cost > dist[node]:
+                continue 
+            for nei,w in adj[node]:
+                newc = w+cost
+                if newc < dist[nei]:
+                    dist[nei] = newc
+                    heapq.heappush(heap,(newc,nei))
         ans = max(dist[1:])
         if ans == float('inf'):
             return -1
