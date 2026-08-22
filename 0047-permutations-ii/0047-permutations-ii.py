c5b62a91a297212fe5d25c8d1690c613dede4e72
tanishaj0804 <atanishajain08@gmail.com>
2026-08-22 11:45:54 +0530

    Time: 124 ms (12.13%), Space: 41.8 MB (5.36%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,24 +1,14 @@
-class Solution(object):
-    def permuteUnique(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        ans = []
-
-        def permute(id):
-            if id == len(nums):
-                ans.append(nums[:])
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def perms(i):
+            if i==len(nums):
+                res.append(nums[:])
                 return
-            used = set()
-            for i in range(id,len(nums)):
-                if nums[i] in used:
-                    continue
-                used.add(nums[i])
-                nums[id], nums[i] = nums[i], nums[id]
-                permute(id+1)
-                nums[id], nums[i] = nums[i], nums[id]
-        permute(0)
-        return ans
-
-        
+            for j in range(i,len(nums)):
+                nums[i],nums[j] = nums[j],nums[i]
+                perms(i+1)
+                nums[i],nums[j] = nums[j],nums[i]
+        perms(0)
+        unique = [list(row) for row in set(tuple(row) for row in res)]
+        return unique
