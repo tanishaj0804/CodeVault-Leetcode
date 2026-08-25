5f168b9a3adf34219a677b8a23daf41ca05653f2
tanishaj0804 <atanishajain08@gmail.com>
2026-08-25 15:11:29 +0530

    Time: 4 ms (36.43%), Space: 19.4 MB (12.69%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,33 +1,26 @@
-class Solution(object):
-    def orangesRotting(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        rows = len(grid)
-        cols = len(grid[0])
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
         fresh = 0
-        mins = 0
-        visited = [[False]*cols for _ in range(rows)]
-        for i in range(rows):
-            for j in range(cols):
+        minutes = 0
+        directions = [(1,0),(-1,0),(0,-1),(0,1)]
+        visited = [[False]*len(grid[0]) for _ in range(len(grid))]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    fresh += 1
                 if grid[i][j] == 2:
                     q.append((i,j))
-                elif grid[i][j] == 1:
-                    fresh += 1
         if fresh == 0:
-            return 0
-        directions = [(-1,0),(1,0),(0,1),(0,-1)]
+            return minutes
         while q:
-                for _ in range(len(q)):
-                    r,c = q.popleft()   #pop from beginning not end 
-                    for dr,dc in directions:
-                        nr = r+dr
-                        nc = c+dc
-                        if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == 1 and not visited[nr][nc]:
-                            visited[nr][nc] = True
-                            q.append((nr,nc))
-                            fresh -= 1
-                mins += 1
-        return mins-1 if fresh == 0 else -1    
+            for _ in range(len(q)):
+                r,c = q.popleft()
+                for dr,dc in directions:
+                    nr,nc = r+dr,c+dc
+                    if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and grid[nr][nc] == 1 and not visited[nr][nc]:
+                        visited[nr][nc] = True
+                        q.append((nr,nc))
+                        fresh -= 1
+            minutes += 1
+        return minutes-1 if fresh == 0 else -1       
