fad08d91e6982dc2be04f9e223ee65f4fcaa9c65
tanishaj0804 <atanishajain08@gmail.com>
2026-08-27 05:25:37 +0530

    Time: 179 ms (56.28%), Space: 14.7 MB (90.42%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,21 +1,26 @@
-class Solution:
-    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        row = len(mat)
-        col = len(mat[0])
+from collections import deque
+class Solution(object):
+    def updateMatrix(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        rows = len(mat)
+        cols = len(mat[0])
         q = deque()
-        for i in range(row):
-            for j in range(col):
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        for i in range(rows):
+            for j in range(cols):
                 if mat[i][j] == 0:
                     q.append((i,j))
                 else:
                     mat[i][j] = -1
-        direction = [(0,1),(0,-1),(1,0),(-1,0)]
         while q:
             r,c = q.popleft()
-            for dr,dc in direction:
-                nr,nc = r+dr,c+dc
-                if 0<=nr<row and 0<=nc<col and mat[nr][nc] == -1:
-                    mat[nr][nc] = mat[r][c] + 1
+            for dr,dc in directions:
+                nr,nc = dr+r,dc+c
+                if 0<=nr<rows and 0<=nc<cols and mat[nr][nc] == -1:
+                    mat[nr][nc] = mat[r][c]+1
                     q.append((nr,nc))
         return mat
 
