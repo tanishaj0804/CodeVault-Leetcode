a9ecca553af009c31fe634188ad9de62c629361d
tanishaj0804 <atanishajain08@gmail.com>
2026-08-26 17:24:24 +0530

    Time: 627 ms (62.2%), Space: 22.3 MB (31.8%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,33 +1,26 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        result = []
+        res = []
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i> 0  and nums[i] == nums[i-1]:
                 continue
-            fixed = nums[i]
-            left = i+1
-            right = len(nums) - 1
-            while left < right:
-                target = fixed + nums[left] +nums[right]
-                if target < 0:
-                    left += 1
-                elif target > 0:
-                    right -= 1
+            l = i+1
+            r = len(nums)-1
+            while l<r:
+                total = nums[i]+nums[l]+nums[r]
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
                 else:
-                    result.append([fixed, nums[left],nums[right]])
-                    left += 1
-                    right -= 1
+                    res.append([nums[i],nums[l],nums[r]])
+                    l += 1
+                    r -= 1
 
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-        return result
+                    while l<r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l<r and nums[r] == nums[r+1]:
+                        r -= 1
 
-
-        
+        return res
