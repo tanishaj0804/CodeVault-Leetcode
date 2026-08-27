cdc50fe20890f39db5bdc8b1e8b824f5f2bcc5e0
tanishaj0804 <atanishajain08@gmail.com>
2026-08-27 05:55:16 +0530

    Time: 639 ms (14.62%), Space: 24.5 MB (68.92%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,30 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def add(self, key: int) -> None:
+        if key in self.stack:
+            return
+        self.stack.append(key)
+
+        
+
+    def remove(self, key: int) -> None:
+        if key in self.stack:
+            self.stack.remove(key)
+        
+
+    def contains(self, key: int) -> bool:
+        if key in self.stack:
+            return True
+        return False
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
