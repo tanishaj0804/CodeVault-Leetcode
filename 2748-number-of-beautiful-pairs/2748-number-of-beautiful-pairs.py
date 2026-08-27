91e88fcada6b78ee77d0a10c7673f4210c60f303
tanishaj0804 <atanishajain08@gmail.com>
2026-08-28 00:26:00 +0530

    Time: 359 ms (5.28%), Space: 20.1 MB (6%) - LeetHub

---
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBeautifulPairs(self, nums: List[int]) -> int:
+        def gcd(a,b):
+            return a if b == 0 else gcd(b,a%b)
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if gcd(int(str(nums[i])[0]),int(str(nums[j])[-1])) == 1:
+                    count += 1
+        return count
+        
