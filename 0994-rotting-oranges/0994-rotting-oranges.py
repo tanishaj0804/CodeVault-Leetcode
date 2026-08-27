7350b5bc3146e536c49517688877a2d8da326304
tanishaj0804 <atanishajain08@gmail.com>
2026-08-27 05:32:57 +0530

    Time: 7 ms (44.88%), Space: 12.3 MB (92.9%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,26 +1,32 @@
-class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
+class Solution(object):
+    def orangesRotting(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        rows = len(grid)
+        cols = len(grid[0])
         q = deque()
         fresh = 0
         minutes = 0
-        directions = [(1,0),(-1,0),(0,-1),(0,1)]
-        visited = [[False]*len(grid[0]) for _ in range(len(grid))]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    fresh += 1
+        visited = [[False]*cols for _ in range(rows)]
+        directions =[(0,1),(0,-1),(1,0),(-1,0)]
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == 2:
                     q.append((i,j))
+                elif grid[i][j] == 1:
+                    fresh += 1
         if fresh == 0:
-            return minutes
+            return 0
         while q:
             for _ in range(len(q)):
                 r,c = q.popleft()
                 for dr,dc in directions:
-                    nr,nc = r+dr,c+dc
-                    if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and grid[nr][nc] == 1 and not visited[nr][nc]:
+                    nr,nc = dr+r,dc+c
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == 1 and not visited[nr][nc]:
                         visited[nr][nc] = True
                         q.append((nr,nc))
                         fresh -= 1
             minutes += 1
-        return minutes-1 if fresh == 0 else -1       
+        return minutes-1 if fresh==0 else -1
