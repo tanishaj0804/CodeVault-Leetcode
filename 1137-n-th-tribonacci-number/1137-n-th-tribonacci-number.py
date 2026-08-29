755d3d99666f9d13304cb422c5f37627520a8277
tanishaj0804 <atanishajain08@gmail.com>
2026-08-29 23:26:12 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.09%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -4,10 +4,7 @@ class Solution:
             return 0
         if n == 1 or n == 2:
             return 1
-        dp = [0]*(n+1)
-        dp[0] = 0
-        dp[1] = dp[2] = 1
+        t0,t1,t2 = 0,1,1
         for i in range(3,n+1):
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
-        return dp[n]
-        
+            t0,t1,t2 = t1,t2,t0+t1+t2
+        return t2
