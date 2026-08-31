3845a13e6d1b78dba34e2ca24c62a0a02a95cefd
tanishaj0804 <atanishajain08@gmail.com>
2026-08-31 19:04:26 +0530

    Time: 7 ms (72.21%), Space: 21.1 MB (49.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,7 +1,7 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
-        l = 0
+        l =0
         r = n-1
         leftm = rightm = 0
         ans = 0
@@ -9,11 +9,11 @@ class Solution:
             leftm = max(leftm,height[l])
             rightm = max(rightm,height[r])
             if leftm < rightm:
-                ans += leftm-height[l]
+                ans += leftm - height[l]
                 l += 1
             else:
                 ans += rightm - height[r]
-                r-=1
+                r -= 1
         return ans
 
         
