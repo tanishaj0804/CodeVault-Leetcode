65ea14a984394ce6d8f0431250db0d5177df85dd
tanishaj0804 <atanishajain08@gmail.com>
2026-09-02 22:41:11 +0530

    Time: 7 ms (70.97%), Space: 20.9 MB (28.63%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,27 +1,21 @@
-class Solution(object):
-    def findCheapestPrice(self, n, flights, src, dst, k):
-        """
-        :type n: int
-        :type flights: List[List[int]]
-        :type src: int
-        :type dst: int
-        :type k: int
-        :rtype: int
-        """
-        adj =[[] for _ in range(n)]
-        for source, destination, price in flights:
-            adj[source].append((destination,price))
-        heap = [(0,src,0)]   #[cost,city,stops]
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = [[] for _ in range(n)]
+        for u,v,cost in flights:
+            adj[u].append((v,cost))
+        dist = [float('inf')]*n
+        dist[src] = 0
         visited = {}
+        heap = [[0,src,0]]   #cost,destination,k
         while heap:
-            cost,city,stops = heapq.heappop(heap)
+            cost, city,stop = heapq.heappop(heap)
             if city == dst:
                 return cost
-            if stops > k:
+            if stop > k:
                 continue
-            if city in visited and visited[city] <= stops:
+            if city in visited and visited[city] <= stop:
                 continue
-            visited[city] = stops
+            visited[city] = stop
             for nex,pric in adj[city]:
-                heapq.heappush(heap,(cost+pric,nex,stops+1))
+                heapq.heappush(heap,(cost+pric,nex,stop+1))
         return -1
