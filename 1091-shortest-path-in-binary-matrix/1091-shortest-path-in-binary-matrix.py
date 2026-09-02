1820e8d8699c03019d49a2a813f53e1ac188d688
tanishaj0804 <atanishajain08@gmail.com>
2026-09-02 18:03:56 +0530

    Time: 222 ms (56.98%), Space: 12.6 MB (83.19%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,21 +1,27 @@
-class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n = len(grid)
+class Solution(object):
+    def shortestPathBinaryMatrix(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        n = len(grid) 
         if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
         if grid[0][0] == 0 and n==1:
             return 1
-        q = deque([(0,0,1)])
         visited = [[False]*n for _ in range(n)]
-        directions = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+        direction = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+        q = deque()
+        q.append((0,0,1))
         while q:
-            r,c,l = q.popleft()
-            if r == n-1 and c ==n-1:
+            x,y,l = q.popleft()
+            if x == n-1 and y == n-1:
                 return l
-            for dr,dc in directions:
-                nr,nc = r+dr,c+dc
-                if 0<=nr<n and 0<=nc<n and grid[nr][nc] != 1:
-                    if not visited[nr][nc]:
-                        visited[nr][nc] = True
-                        q.append((nr,nc,l+1))        
+            for dr,dc in direction:
+                nr,nc = dr+x,dc+y
+                if 0<=nr<n and 0<=nc<n and grid[nr][nc] != 1 and not visited[nr][nc]:
+                    visited[nr][nc] = True
+                    q.append((nr,nc,l+1))
         return -1
+        
+        
