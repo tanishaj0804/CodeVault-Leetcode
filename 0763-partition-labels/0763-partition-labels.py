d9f7eead07be9dbdc5f11207a05a848e4b077d46
tanishaj0804 <atanishajain08@gmail.com>
2026-09-05 12:33:21 +0530

    Time: 2 ms (88.71%), Space: 19.3 MB (25.51%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,13 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last= {}
+        res = []
+        for i in range(len(s)):
+            last[s[i]] = i  #stores last index of each character
+        start = end = 0
+        for i in range(len(s)):
+            end = max(end,last[s[i]])
+            if i == end:
+                res.append(end-start+1)
+                start = i+1
+        return res
