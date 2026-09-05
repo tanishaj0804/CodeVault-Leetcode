3b076c4e0836bde121e0bee1b45d00d604dc5ba3
tanishaj0804 <atanishajain08@gmail.com>
2026-09-05 15:28:49 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.7%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        cmin = cmax = 0
+        for ch in s:
+            cmin = cmin+1 if ch == "(" else max(cmin-1,0)
+            cmax = cmax-1 if ch == ')' else cmax+1
+            if cmax<0:
+                return False
+        return cmin== 0
+            
