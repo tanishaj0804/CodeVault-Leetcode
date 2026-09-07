1f27b0c2c5960051c6c7ba7a841055911a44390f
tanishaj0804 <atanishajain08@gmail.com>
2026-09-07 13:04:20 +0530

    Time: 587 ms (7.2%), Space: 19.8 MB (37.72%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,38 +1,31 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
         freq = Counter(t)
         window = {}
-        if s == t:
-            return t
         if len(s) < len(t):
             return ""
-        def valid():
+        if s == t:
+            return s
+        i = j = 0
+        minv = float('inf')
+        ans = ""
+        def isvalid():
             for ch in freq:
                 if window.get(ch,0) < freq[ch]:
                     return False
             return True
-        l = 0
-        r = 0
-        minv = float('inf')
-        ans = ""
-        while r < len(s):
-            ch = s[r]
-            window[ch] = window.get(ch,0) + 1
-            while valid():
-                if r-l+1 < minv:
-                    minv = r-l+1
-                    ans = s[l:r+1]
-                window[s[l]] -= 1
-                if window[s[l]] == 0:
-                    del window[s[l]]
-                l += 1
-            r += 1
+        while j < len(s):
+            ch = s[j]
+            window[ch] = window.get(ch,0)+1
+            while isvalid():
+                if j-i+1 < minv:
+                    minv = j-i+1
+                    ans = s[i:j+1]
+                window[s[i]] -= 1
+                if window[s[i]] == 0:
+                    del window[s[i]]
+                i += 1
+            j += 1
         return ans
-
-
         
+            
