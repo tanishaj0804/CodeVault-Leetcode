9ac07022c5efdb9aad08a6ae28bb94596566c575
tanishaj0804 <atanishajain08@gmail.com>
2026-09-11 00:27:36 +0530

    Time: 28 ms (45.08%), Space: 33.5 MB (50.23%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         #Floyds Cycle Detection
-        slow  = fast = nums[0]
+        slow = fast = nums[0]
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
